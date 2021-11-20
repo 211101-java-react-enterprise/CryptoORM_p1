76a8722215a7112e447ffda46506fa2d1e7f0708
Andrew Aslakson <andrew01177@gmail.com>
2021-11-19 20:06:16 -0500

    Setting up dev branch

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Driver {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
+
+        Class classType = ArrayList.class;
     }
 }
