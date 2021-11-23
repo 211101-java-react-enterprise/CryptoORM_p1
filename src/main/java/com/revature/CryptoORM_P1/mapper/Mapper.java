df696ffd9138e6764c9bbcf1020ad9e2eb3d4632
Andrew Aslakson <andrew01177@gmail.com>
2021-11-23 16:11:37 -0500

    Added custom exception and begin work on Mapper#insert

--- src/main/java/com/revature/CryptoORM_P1/mapper/Mapper.java
+++ src/main/java/com/revature/CryptoORM_P1/mapper/Mapper.java
@@ -1,10 +1,27 @@
 package com.revature.CryptoORM_P1.mapper;
 
+import com.revature.CryptoORM_P1.annotations.Table;
+import com.revature.CryptoORM_P1.exception.InvalidClassException;
+
+import java.lang.reflect.Field;
+
 public class Mapper {
 
     StringBuilder builder = new StringBuilder("");
 
-    public boolean insert(Object obj) {
+    public boolean insert(Object obj) throws InvalidClassException {
+
+        // Store necessary data from object
+        Class inputClass = obj.getClass();
+        Table table;
+        Field[] fields;
+
+        if (inputClass.isAnnotationPresent(Table.class)) {
+            table = (Table)inputClass.getAnnotation(Table.class);
+        } else {
+            throw new InvalidClassException("Class provided is missing Table annotation!");
+        }
+
 
         return false;
     }
