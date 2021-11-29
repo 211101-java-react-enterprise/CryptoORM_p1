a9361a18c5600768f961f6e63081b62d9460bcd2
Andrew Aslakson <andrew01177@gmail.com>
2021-11-29 11:41:57 -0500

    Refactored some Mapping Logic and added update method

--- src/main/java/com/revature/CryptoORM_P1/mapper/SQLMapper.java
+++ src/main/java/com/revature/CryptoORM_P1/mapper/SQLMapper.java
@@ -5,15 +5,12 @@ import com.revature.CryptoORM_P1.annotations.Table;
 import com.revature.CryptoORM_P1.annotations.Value;
 import com.revature.CryptoORM_P1.exception.InvalidClassException;
 import com.revature.CryptoORM_P1.exception.MethodInvocationException;
-import com.revature.CryptoORM_P1.models.User;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Collector;
 
 public class SQLMapper {
 
@@ -23,55 +20,24 @@ public class SQLMapper {
 
         // Store necessary data from object
         Class inputClass = obj.getClass();
-        Table table;
-        Field[] fields;
-        Method[] methods;
+        Table table = getTable(inputClass);
 
-        ArrayList<Column> columns = new ArrayList<>();
-
-        if (inputClass.isAnnotationPresent(Table.class)) {
-            table = (Table)inputClass.getAnnotation(Table.class);
-        } else {
-            throw new InvalidClassException("Class provided is missing Table annotation!");
-        }
-
-        fields = inputClass.getDeclaredFields();
-
-        methods = Arrays.stream(inputClass.getMethods())
-                                        .filter(m -> m.isAnnotationPresent(Value.class))
-                                        .toArray(Method[]::new);
-
-        for (Field field : fields) {
-            if (field.isAnnotationPresent(Column.class)) {
-                columns.add(field.getAnnotation(Column.class));
-            }
-        }
-
-        if (columns.isEmpty()) {
-            throw new InvalidClassException("Class provided has no Column Annotations!");
-        }
+        ArrayList<ArrayList<String>> columnData = getColumnsAndValues(obj);
 
         builder.setLength(0);
 
         builder.append("insert into " + table.tableName() + " (");
-        for (Column column : columns) {
-            builder.append(column.columnName() + ", ");
+
+        for (String column : columnData.get(0)) {
+            builder.append(column + ", ");
         }
+
         builder.setLength(builder.length() - 2);
+
         builder.append(") values ");
-        for (int i = 0; i < methods.length; i++) {
-            try {
-                for (Method method : methods) {
-                    if (method.getAnnotation(Value.class).correspondingColumn()
-                                .equals(fields[i].getAnnotation(Column.class).columnName())) {
-                        builder.append(method.invoke(obj) + ", ");
-                        break;
-                    }
-                }
-            }  catch (InvocationTargetException | IllegalAccessException e) {
-                e.printStackTrace();
-                throw new MethodInvocationException("There was an error when attempting to invoke obj's methods");
-            }
+
+        for (String value : columnData.get(1)) {
+            builder.append("'" + value + "', ");
         }
         builder.setLength(builder.length() - 2);
         builder.append(";");
@@ -80,5 +46,95 @@ public class SQLMapper {
 
         return true;
     }
+
+    public boolean update(Object obj, String idColumnName) {
+
+        // Store necessary data from object
+        Class inputClass = obj.getClass();
+        Table table = getTable(inputClass);
+
+        ArrayList<ArrayList<String>> columnData = getColumnsAndValues(obj);
+
+        builder.setLength(0);
+
+        builder.append("update " + table.tableName() + " set ");
+
+        for (int i = 0; i < columnData.get(0).size(); i++) {
+            builder.append(columnData.get(0).get(i) + " = '" + columnData.get(1).get(i) + "', ");
+        }
+
+        builder.setLength(builder.length() - 2);
+
+        builder.append(" where " + idColumnName + " = '");
+        for (int i = 0; i < columnData.get(0).size(); i++) {
+            if (columnData.get(0).get(i).equals(idColumnName)) {
+                builder.append(columnData.get(1).get(i));
+                break;
+            }
+        }
+
+        builder.append("';");
+
+        System.out.println(builder);
+
+        return true;
+    }
+
+    private Table getTable(Class inputClass) {
+        if (inputClass.isAnnotationPresent(Table.class)) {
+            return (Table)inputClass.getAnnotation(Table.class);
+        } else {
+            throw new InvalidClassException("Class provided is missing Table annotation!");
+        }
+
+    }
+
+    /**
+     *      Takes in obj and returns an arraylist of arraylist of string where
+     *      arraylist.get(0) = columnName, and arraylist.get(1) = value
+     * @param obj
+     * @return
+     */
+    private ArrayList<ArrayList<String>> getColumnsAndValues(Object obj) {
+
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        result.add(new ArrayList<>());
+
+        Class inputClass = obj.getClass();
+
+        Field[] fields = inputClass.getDeclaredFields();
+
+        Method[] methods = Arrays.stream(inputClass.getMethods())
+                .filter(m -> m.isAnnotationPresent(Value.class))
+                .toArray(Method[]::new);
+
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Column.class)) {
+                result.get(0).add(field.getAnnotation(Column.class).columnName());
+            }
+        }
+
+        if (result.get(0).isEmpty()) {
+            throw new InvalidClassException("Class provided has no Column Annotations!");
+        }
+
+        for (int i = 0; i < methods.length; i++) {
+            try {
+                for (Method method : methods) {
+                    if (method.getAnnotation(Value.class).correspondingColumn()
+                            .equals(result.get(0).get(i))) {
+                        result.get(1).add(method.invoke(obj).toString());
+                        break;
+                    }
+                }
+            }  catch (InvocationTargetException | IllegalAccessException e) {
+                e.printStackTrace();
+                throw new MethodInvocationException("There was an error when attempting to invoke obj's methods");
+            }
+        }
+        return result;
+    }
+
 
 }
